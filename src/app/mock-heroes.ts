73b935cf1c4b5c3d9886f09b0af415ced36470fa
marcilioreis <emaildomarcilio@gmail.com>
2018-03-17 14:50:54 -0300

    Add files via upload

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,29 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Rogerinho do Ingá' },
+  { id: 2, name: 'Julinho da Van' },
+  { id: 3, name: 'Maurílio' },
+  { id: 4, name: 'Renan' },
+  { id: 5, name: 'Magia Top' },
+  { id: 6, name: 'Atura o Parabéns' },
+  { id: 7, name: 'Dorival' },
+  { id: 8, name: 'Jambalaya' },
+  { id: 9, name: 'Q' },
+  { id: 10, name: 'Teu Cu' },
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' },
+  { id: 21, name: 'Alabama' },
+  { id: 22, name: 'Glub Glub' },
+  { id: 23, name: 'Facade' },
+  { id: 24, name: 'Ch4v3' },
+  { id: 25, name: 'Au Au' }
+];
